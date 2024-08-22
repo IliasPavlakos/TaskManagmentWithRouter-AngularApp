96650001c6da9aa72578e50de83f887864d1bc32
ipavlakos <pavlakosIlias@outlook.com>
2024-08-22 21:41:11 +0300

    Setting query parameters

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -4,13 +4,14 @@ import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
 import { CardComponent } from '../../shared/card/card.component';
 import { TasksService } from '../tasks.service';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-task',
   standalone: true,
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
-  imports: [DatePipe, CardComponent],
+  imports: [DatePipe, CardComponent, RouterLink],
 })
 export class TaskComponent {
   task = input.required<Task>();
